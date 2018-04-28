3f201b85058a1c718ddb6f6da786cbd0d33c57ab
Kasper Johansen <kaspernj@gmail.com>
2018-04-28 18:21:34 +0200

    Update 20170217082955_add_host_protocol_and_port_to_jobler_jobs.rb

--- db/migrate/20170217082955_add_host_protocol_and_port_to_jobler_jobs.rb
+++ db/migrate/20170217082955_add_host_protocol_and_port_to_jobler_jobs.rb
@@ -1,4 +1,4 @@
-class AddHostProtocolAndPortToJoblerJobs < ActiveRecord::Migration
+class AddHostProtocolAndPortToJoblerJobs < ActiveRecord::Migration[5.1]
   def change
     add_column :jobler_jobs, :host, :string
     add_column :jobler_jobs, :protocol, :string
