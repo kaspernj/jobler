8a71fc4303cddd6625cd213f4cb886a19e43fa4a
Kasper Johansen <kaspernj@gmail.com>
2020-03-15 15:34:06 +0100

    Fixed create result with content (#51)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,7 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each { |f| require f }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.maintain_test_schema!
+# ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include ActiveJob::TestHelper
