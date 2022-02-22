944543fb99f96d89858d8b3571b70728b4c3b776
kaspernj <k@spernj.org>
2022-02-22 09:37:43 +0000

    Rails 7 fixes

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -15,6 +15,12 @@ require "jobler"
 module Dummy; end
 
 class Dummy::Application < Rails::Application
+  if Gem.loaded_specs["rails"].version.to_s.start_with?("7.")
+    config.load_defaults 7.0
+  else
+    config.load_defaults 6.0
+  end
+
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
